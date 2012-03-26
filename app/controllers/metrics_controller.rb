df277200f604451b860770b76dc5c3c23b772406
Abhinav Sahai <as5908@nyu.edu>
2012-03-25 23:55:59 -0400

    updated production.rb 2

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -3,7 +3,6 @@ class MetricsController < ApplicationController
   # GET /metrics.json
   def index
     @metrics = Metric.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @metrics }
@@ -37,37 +36,6 @@ class MetricsController < ApplicationController
     @metric = Metric.find(params[:id])
   end
 
-  # POST /metrics
-  # POST /metrics.json
-  def create
-    @metric = Metric.new(params[:metric])
-
-    respond_to do |format|
-      if @metric.save
-        format.html { redirect_to @metric, notice: 'Metric was successfully created.' }
-        format.json { render json: @metric, status: :created, location: @metric }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @metric.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /metrics/1
-  # PUT /metrics/1.json
-  def update
-    @metric = Metric.find(params[:id])
-
-    respond_to do |format|
-      if @metric.update_attributes(params[:metric])
-        format.html { redirect_to @metric, notice: 'Metric was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @metric.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /metrics/1
   # DELETE /metrics/1.json
