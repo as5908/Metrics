6f442447c4cd16abb7ede316e5371837e91cc6f8
Abhinav Sahai <as5908@nyu.edu>
2012-03-25 23:28:40 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metrics::Application.config.secret_token = '562531f8414289d3f785a03606992549c35774d1c67808a7dd0a0d0c9cddcff85542a0de81630217e38506d36887467688b021a3107e031bdff72d4d65adbbf8'
